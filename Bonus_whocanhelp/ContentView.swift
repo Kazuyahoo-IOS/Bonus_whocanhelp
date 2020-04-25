b2c04506426e7a7bcb51c420dce98401d8e0ba47
kazuyahooo <46344542+kazuyahooo@users.noreply.github.com>
2020-04-25 19:29:14 +0800

    Update

--- Bonus_whocanhelp/ContentView.swift
+++ Bonus_whocanhelp/ContentView.swift
@@ -7,10 +7,63 @@
 //
 
 import SwiftUI
-
+struct pic {
+    var img: String
+    var name: String
+    var description: String
+}
 struct ContentView: View {
+    let roles: [pic] = [
+        pic(img: "1", name: "1", description: "SpiderCat"),
+        pic(img: "2", name: "2", description: "DeadPoolCat"),
+        pic(img: "3", name: "3", description: "VenomCat"),
+        pic(img: "4", name: "4", description: "ThanosCat"),
+        pic(img: "5", name: "5", description: "MarvelCat")
+    ]
+    
+    @State private var pickindex = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            
+            Image("background")
+                .resizable()
+                .scaledToFill()
+            
+            HStack{
+                Button(action:{
+                    let randNum = 0 ..< self.roles.count
+                    self.pickindex = randNum.randomElement()!
+                }){
+                    Image(systemName:"questionmark.square.fill")
+                        .font(.system(size: 40))
+                        .padding()
+                }
+                
+                Picker(selection: $pickindex, label: Text("")) {
+                    ForEach(0 ..< roles.count) { i in
+                        Text(self.roles[i].name).tag(i)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .background(Color.gray)
+                .border(Color.white)
+                .frame(width:250)
+            }.offset(y:70)
+            
+            Image("\(roles[pickindex].img)")
+                .resizable()
+                .scaledToFit()
+                .frame(width:300)
+                .offset(x:-50,y:280)
+            
+            Text("\(roles[pickindex].description)")
+                .offset(x:0,y:-60)
+                .font(.system(size: 38))
+                .foregroundColor(Color.white)
+            
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
